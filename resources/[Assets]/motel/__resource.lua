5ae108e9dd3cc0e31991850dbc1e82ca5a61553b
WastedLight <77549673+WastedLight@users.noreply.github.com>
2022-05-28 08:45:16 +0100

    .

---
+++ resources/[Assets]/motel/__resource.lua
@@ -0,0 +1,26 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+this_is_a_map 'yes'
+
+files {
+    "interiorproxies.meta",
+    'shellpropsv9.ytyp',
+    'shellprops.ytyp',
+    'shellpropv2s.ytyp',
+    'shellpropsv5.ytyp',
+    'shellpropsv10.ytyp',
+    'shellpropsv4.ytyp',
+    'v_int_shop.ytyp',
+    'playerhouse_hotel/playerhouse_hotel.ytyp',
+}
+
+data_file 'DLC_ITYP_REQUEST' 'shellpropsv9.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'shellprops.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'shellpropsv2.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'shellpropsv5.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'shellpropsv10.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'shellpropsv4.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'v_int_20.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'v_int_shop.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'playerhouse_hotel/playerhouse_hotel.ytyp'
+data_file "INTERIOR_PROXY_ORDER_FILE" "interiorproxies.meta"
